133a02a8ef4e07ea791d57b2a2178baec18b15e2
NicolasCirulli <nicolascirulli2@gmail.com>
2022-11-30 11:49:48 -0300

    workshop de flor

---
+++ main.js
@@ -0,0 +1,97 @@
+// let peligro = false
+// let bandera = 'roja'
+// let compraUno = prompt('Ingresa tu primer compra')
+// let compraDos = prompt('Ingresa tu segunda compra')
+//  let conMayor = prompt('Vienes con un mayor?')
+// let miNombre = prompt('Ingresa tu nombre')
+// let idioma = prompt('Ingresa tu idioma')
+
+// if (edad > 10) {
+//     console.log('Sos mayor a 10')
+// }
+
+// if (miNombre == 'flor') {
+//     console.log('te llamas flor')
+// }
+// if (edad > 4) {
+//     console.log('Tenes mas de 4 años')
+// }
+// else if (edad < 4) {
+//     console.log('Eres menor a 4 años')
+// }
+// else {
+//     console.log('Tengo 4 años')
+// }
+
+// if (miNombre == 'flor') {
+//     console.log('pasas gratis porque te llamas flor')
+// }
+// else if (miNombre == 'nico') {
+//     console.log('Tenes descuento porque te llamas nico')
+// }
+// else {
+//     console.log('Tenés que comprar una entrada a 200 pesos')
+// }
+
+// switch (idioma) {
+//     case "Inglés":
+//         console.log('Welcome')
+//         break;
+//     case "Japonés":
+//         console.log('Okaeri') 
+//         break;
+//     case "Italiano":
+//         console.log('Ben Arrivato') 
+//         break;
+    
+//     default:
+//         console.log('Bienvenido')
+// }
+
+//&& 
+// true && true = true
+// true && false = false
+// false && false = false
+
+// if ((edad <= 17) && (mayor == 'si')) {
+//     console.log('No puedes pasar, solo mayores de edad')
+// }
+
+// || 
+// true || true = true
+// true || false = true
+// false || false = false
+
+// if ((edad >= 18 ) || (conMayor == 'si')) {
+//     console.log('podes pasar')
+// }
+// else {
+//     console.log('no podes pasar')
+// }
+
+// !
+// true && !true = false 
+// !true = false
+// !false && !false = true
+
+// if (edad !== 2) {
+//     console.log('no es igual a 2')
+// }
+// if (!(edad > 2)) {
+//     console.log('Eres menor a 2')
+// }
+// if ((bandera == 'roja') && !peligro) {
+//     console.log('No puedes pasar')
+// }
+
+// Yo quiero hacer un budin
+
+// if ((compraUno == 'pan') && (compraDos == 'leche')) {
+//     console.log('Compraste ambas cosas')
+// }
+// else if ((compraUno == 'pan') || (compraDos == 'leche')) {
+//     console.log('compraste uno de los ingredientes')
+// }
+// else {
+//     console.log('No compraste nada')
+// }
